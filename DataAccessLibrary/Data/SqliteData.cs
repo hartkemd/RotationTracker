d8a27ee38f542be948dd6b7c7409d629f15d69c0
Mark Hartkemeyer <hartkemd@gmail.com>
2022-02-20 11:14:21 -0500

    Changed data access to asynchronous.

--- DataAccessLibrary/Data/SqliteData.cs
+++ DataAccessLibrary/Data/SqliteData.cs
@@ -23,21 +23,21 @@ namespace DataAccessLibrary.Data
         {
             string sql = "SELECT Id, FullName FROM Employees ORDER BY FullName;";
 
-            return _db.LoadData<EmployeeModel, dynamic>(sql, new { }, connectionStringName);
+            return _db.LoadDataAsync<EmployeeModel, dynamic>(sql, new { }, connectionStringName).Result;
         }
 
         public void CreateEmployee(string employeeName)
         {
             string sql = "INSERT INTO Employees (FullName) VALUES(@FullName);";
 
-            _db.SaveData(sql, new { FullName = employeeName }, connectionStringName);
+            _db.SaveDataAsync(sql, new { FullName = employeeName }, connectionStringName);
         }
 
         public void DeleteEmployee(int id)
         {
             string sql = "DELETE FROM Employees WHERE Id = @Id;";
 
-            _db.SaveData(sql, new { Id = id }, connectionStringName);
+            _db.SaveDataAsync(sql, new { Id = id }, connectionStringName);
         }
 
         public void CreateRotation(FullRotationModel fullRotation)
@@ -45,7 +45,7 @@ namespace DataAccessLibrary.Data
             string sql = "INSERT INTO Rotations (RotationName, RotationRecurrence, NextDateTimeRotationAdvances, AdvanceAutomatically, Notes) " +
                             "VALUES(@RotationName, @RecurrenceInterval, @NextDateTimeRotationAdvances, @AdvanceAutomatically, @Notes);";
 
-            _db.SaveData(sql, new { RotationName = fullRotation.BasicInfo.RotationName,
+            _db.SaveDataAsync(sql, new { RotationName = fullRotation.BasicInfo.RotationName,
                                     RecurrenceInterval = fullRotation.BasicInfo.RotationRecurrence,
                                     NextDateTimeRotationAdvances = fullRotation.BasicInfo.NextDateTimeRotationAdvances,
                                     AdvanceAutomatically = fullRotation.BasicInfo.AdvanceAutomatically,
@@ -54,14 +54,14 @@ namespace DataAccessLibrary.Data
 
             sql = "SELECT Id FROM Rotations ORDER BY Id DESC LIMIT 1;"; // select the id of the row just inserted
 
-            int rotationId = _db.LoadData<int, dynamic>(sql, new { }, connectionStringName).First();
+            int rotationId = _db.LoadDataAsync<int, dynamic>(sql, new { }, connectionStringName).Result.First();
 
             for (int i = 0; i < fullRotation.RotationOfEmployees.Count; i++)
             {
                 sql = "INSERT INTO RotationEmployees (RotationId, EmployeeId, Position, OnCalendar) " +
                         "VALUES(@RotationId, @EmployeeId, @Position, 0);";
 
-                _db.SaveData(sql, new { RotationId = rotationId,
+                _db.SaveDataAsync(sql, new { RotationId = rotationId,
                                         EmployeeId = fullRotation.RotationOfEmployees[i].Id,
                                         Position = i },
                                         connectionStringName);
@@ -74,7 +74,7 @@ namespace DataAccessLibrary.Data
 
             string sql = "DELETE FROM RotationEmployees WHERE RotationId = @RotationId;";
 
-            _db.SaveData(sql, new { RotationId = rotationId }, connectionStringName);
+            _db.SaveDataAsync(sql, new { RotationId = rotationId }, connectionStringName);
 
             int position = 0;
             foreach (var employee in fullRotation.RotationOfEmployees)
@@ -82,7 +82,7 @@ namespace DataAccessLibrary.Data
                 sql = "INSERT INTO RotationEmployees(RotationId, EmployeeId, Position, OnCalendar) " +
                         "VALUES(@RotationId, @EmployeeId, @Position, 0);";
 
-                _db.SaveData(sql, new { RotationId = rotationId,
+                _db.SaveDataAsync(sql, new { RotationId = rotationId,
                                         EmployeeId = employee.Id,
                                         Position = position },
                                         connectionStringName);
@@ -97,7 +97,7 @@ namespace DataAccessLibrary.Data
 
             string sql = "SELECT Id FROM Rotations;";
 
-            List<int> rotationIds = _db.LoadData<int, dynamic>(sql, new { }, connectionStringName);
+            List<int> rotationIds = _db.LoadDataAsync<int, dynamic>(sql, new { }, connectionStringName).Result;
 
             foreach (var id in rotationIds)
             {
@@ -105,14 +105,15 @@ namespace DataAccessLibrary.Data
 
                 sql = "SELECT * FROM Rotations WHERE Id = @Id;";
 
-                fullRotation.BasicInfo = _db.LoadData<BasicRotationModel, dynamic>(sql, new { Id = id }, connectionStringName).First();
+                fullRotation.BasicInfo = _db.LoadDataAsync<BasicRotationModel, dynamic>(sql, new { Id = id }, connectionStringName).Result.First();
 
                 sql = "SELECT e.*, re.OnCalendar FROM Employees e " +
                         "INNER JOIN RotationEmployees re ON e.Id = re.EmployeeId " +
                         "INNER JOIN Rotations r ON re.RotationId = r.Id " +
                         "WHERE r.Id = @Id ORDER BY re.Position;";
 
-                fullRotation.RotationOfEmployees = new ObservableCollection<EmployeeModel>(_db.LoadData<EmployeeModel, dynamic>(sql, new { Id = id }, connectionStringName));
+                fullRotation.RotationOfEmployees = new ObservableCollection<EmployeeModel>
+                    (_db.LoadDataAsync<EmployeeModel, dynamic>(sql, new { Id = id }, connectionStringName).Result);
 
                 output.Add(fullRotation);
             }
@@ -126,7 +127,7 @@ namespace DataAccessLibrary.Data
             
             string sql = "SELECT Id FROM Rotations ORDER BY Id DESC LIMIT 1;";
 
-            output = _db.LoadData<int, dynamic>(sql, new { }, connectionStringName).First();
+            output = _db.LoadDataAsync<int, dynamic>(sql, new { }, connectionStringName).Result.First();
 
             return output;
         }
@@ -137,7 +138,7 @@ namespace DataAccessLibrary.Data
                             "NextDateTimeRotationAdvances = @NextDateTimeRotationAdvances, " +
                             "AdvanceAutomatically = @AdvanceAutomatically, Notes = @Notes WHERE Id = @Id;";
 
-            _db.SaveData(sql, new { basicRotation.RotationName,
+            _db.SaveDataAsync(sql, new { basicRotation.RotationName,
                                     basicRotation.RotationRecurrence,
                                     basicRotation.NextDateTimeRotationAdvances,
                                     basicRotation.AdvanceAutomatically,
@@ -152,14 +153,14 @@ namespace DataAccessLibrary.Data
 
             string sql = "SELECT EmployeeId FROM RotationEmployees WHERE RotationId = @RotationId AND Position = 0;";
 
-            int employeeId = _db.LoadData<int, dynamic>(sql, new { RotationId = rotationId }, connectionStringName).First();
+            int employeeId = _db.LoadDataAsync<int, dynamic>(sql, new { RotationId = rotationId }, connectionStringName).Result.First();
 
             sql = "UPDATE RotationEmployees SET Position = " +
                     "(SELECT count(*) FROM RotationEmployees WHERE RotationId = @RotationId) " +
                     "WHERE EmployeeId = @EmployeeId AND RotationId = @RotationId; " +
                     "UPDATE RotationEmployees SET Position = Position - 1 WHERE RotationId = @RotationId;";
 
-            _db.SaveData(sql, new { RotationId = rotationId, EmployeeId = employeeId }, connectionStringName);
+            _db.SaveDataAsync(sql, new { RotationId = rotationId, EmployeeId = employeeId }, connectionStringName);
         }
 
         public void ReverseRotation(FullRotationModel fullRotation) // moves the rotation back one position in the list
@@ -170,12 +171,12 @@ namespace DataAccessLibrary.Data
             string sql = "SELECT EmployeeId FROM RotationEmployees WHERE RotationId = @RotationId AND Position = " +
                             "(SELECT Position FROM RotationEmployees WHERE RotationId = @RotationId ORDER BY Position DESC LIMIT 1);";
 
-            int employeeId = _db.LoadData<int, dynamic>(sql, new { RotationId = rotationId }, connectionStringName).First();
+            int employeeId = _db.LoadDataAsync<int, dynamic>(sql, new { RotationId = rotationId }, connectionStringName).Result.First();
 
             sql = "UPDATE RotationEmployees SET Position = -1 WHERE RotationId = @RotationId AND EmployeeId = @EmployeeId; " +
                     "UPDATE RotationEmployees SET Position = Position + 1 WHERE RotationId = @RotationId;";
 
-            _db.SaveData(sql, new { EmployeeId = employeeId, RotationId = rotationId }, connectionStringName);
+            _db.SaveDataAsync(sql, new { EmployeeId = employeeId, RotationId = rotationId }, connectionStringName);
         }
 
         public void DeleteRotation(int id)
@@ -183,18 +184,14 @@ namespace DataAccessLibrary.Data
             string sql = "DELETE FROM Rotations WHERE Id = @Id; " +
                             "DELETE FROM RotationEmployees WHERE RotationId = @Id;";
 
-            _db.SaveData(sql, new { Id = id }, connectionStringName);
+            _db.SaveDataAsync(sql, new { Id = id }, connectionStringName);
         }
 
         public List<string> ReadAllAdmins()
         {
-            List<string> output = new ();
-
             string sql = "SELECT UserName FROM Admins;";
 
-            output = _db.LoadData<string, dynamic>(sql, new { }, connectionStringName);
-
-            return output;
+            return _db.LoadDataAsync<string, dynamic>(sql, new { }, connectionStringName).Result;
         }
 
         public void UpdateOnCalendar(BasicRotationModel basicRotation, EmployeeModel employee, bool onCalendar)
@@ -202,7 +199,7 @@ namespace DataAccessLibrary.Data
             string sql = "UPDATE RotationEmployees SET OnCalendar = @OnCalendar " +
                             "WHERE RotationId = @RotationId AND EmployeeId = @EmployeeId;";
 
-            _db.SaveData(sql, new { OnCalendar = onCalendar,
+            _db.SaveDataAsync(sql, new { OnCalendar = onCalendar,
                                     RotationId = basicRotation.Id,
                                     EmployeeId = employee.Id }, connectionStringName);
         }
